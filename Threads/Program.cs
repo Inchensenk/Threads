459dae37fd40592cbb065181bbd9fbd43c007326
sofoyan gevork <aab81@bk.ru>
2022-09-23 14:30:22 +0300

    Добавьте файлы проекта.

---
+++ Threads/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void WriteSecond()
+        {
+            while(true)
+            {
+                Console.WriteLine(new String(' ', 10) + "Вторичный");
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*ThreadStart это делегат, который представляет метод, который выполняется в отношении Thread.
+            Этому делегату передается метод(в данном случае WriteSecond()) который мы хотим запустить в новом потоке*/
+            ThreadStart writeSecond = new ThreadStart(WriteSecond);
+
+            /*thread обьектно ориентированное представление потока в который передаем делегат writeSecond
+            С помощью этого класса можно запускать и управлять потоками*/
+            Thread thread = new Thread(writeSecond);
+
+            //для запуска потока вызываем у экземпляра thread метод Start()
+            thread.Start();
+            while (true)
+            {
+                Console.WriteLine("Первичный");
+            }
+        }
+    }
+}
+
+/*Для каждого потока выделяется 1мб стека, не больше, ни меньше*/
---
+++ Threads1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+
+        static void WriteSecond()
+        {
+            int counter = 0;
+
+            while (counter < 10)
+            {
+                Console.WriteLine($"Thread Id {Thread.CurrentThread.GetHashCode()}, coounter = {counter}");
+                counter++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(WriteSecond));
+            thread.Start();
+
+            WriteSecond();
+            Console.ReadKey();
+        }
+    }
+}
