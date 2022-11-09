507faa0d0b851166a6d563a86c8e6b7356634067
Inchensenk <aab8117@gmail.com>
2022-11-09 17:26:16 +0300

    C#. Потоки / процессор / прерывания. Урок 69 ч. 2
    https://www.youtube.com/watch?v=CYzcwKcF7JU

---
+++ Threads2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads2
+{
+    public class Program
+    {
+        static void WriteSecond()
+        {
+            //CurrentThread содержит ссылку на поток в контексте которого будет запущен метод WriteSecond
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "Вторичный";
+            Console.WriteLine($"Id потока {thread.Name}: {thread.GetHashCode()}");
+
+            for (int counter = 0; counter< 10; counter++)
+            {
+                Console.WriteLine(new string(' ', 10) + thread.Name + " " + counter);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread primaryThread = Thread.CurrentThread;
+           primaryThread.Name  = "Первичный";
+            Console.WriteLine($"Id потока {primaryThread.Name}: {primaryThread.GetHashCode()}");
+
+            Thread secondaryThread = new Thread(WriteSecond);
+            secondaryThread.Start();
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(primaryThread.Name+ " " + counter);
+                Thread.Sleep(1500);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
