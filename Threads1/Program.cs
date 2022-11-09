3cfc7bf7cb4df20cfc235fb71d76c490684e036b
Inchensenk <aab8117@gmail.com>
2022-11-09 16:30:46 +0300

    https://www.youtube.com/watch?v=7M6rpBjh89A&t=11s

--- Threads1/Program.cs
+++ Threads1/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Globalization;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Threads1
 {
     class Program
     {
 
         static void WriteSecond()
         {
+            //счетчик
             int counter = 0;
 
             while (counter < 10)
             {
+                //Вывод Id потока и счетчик цикла
                 Console.WriteLine($"Thread Id {Thread.CurrentThread.GetHashCode()}, coounter = {counter}");
                 counter++;
             }
@@ -26,3 +28,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
     }
 }
+
+/*При запуске программы для каждого потока был свой counter, 
+ * то есть для каждого потока счетчик был от 0 до 9 в зависимости от итерации цикла while
+ 
+Это говорит о том что в каждом потоке как будто бы выполнялась своя копия метода WriteSecond()
+Но на самом деле выполнялась сложная операция пересчета адресов, за которые отвечает система,
+Так же происходят некоторые операции копирования и  в итоге получаются как бы две разные копии одно и того же метода,
+которые работают независимо в разных потоках поэтому у каждого потока свой собственный counter*/
+
