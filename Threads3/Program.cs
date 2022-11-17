e91b3ce95376634234c2806221cdaf74132e6fb1
Inchensenk <aab8117@gmail.com>
2022-11-17 15:40:23 +0300

    https://www.youtube.com/watch?v=e4bys__BCuQ&t=654s

---
+++ Threads3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CriticalSection
+{
+    class MyClass
+    {
+        //объект блокировки(это может быть любой объект)
+        object block = new object();
+        public void Method()
+        {
+            int hash = Thread.CurrentThread.GetHashCode();
+
+            //Критическая секция
+            lock(block)
+            {
+                for (int counter = 0; counter < 10; counter++)
+                {
+                    Console.WriteLine($"Поток (бегун) # {hash}: итерация (круг) {counter}");
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine(new string('-', 37));
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80,40);
+
+            MyClass instance = new MyClass();
+
+            //Создание потоков по слабой ссылке
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(instance.Method).Start();
+            }
+            Thread.Sleep(1000);
+
+            Console.ReadKey();
+        }
+    }
+}
