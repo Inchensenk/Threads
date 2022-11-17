35ea2371beb2043dd6296f4d198e16eb63d9ae37
Inchensenk <aab8117@gmail.com>
2022-11-17 16:23:24 +0300

    Как работает многопоточность и синхронизация потоков / Monitor (ДУШевный пример)
    https://www.youtube.com/watch?v=cfmWid-IZmM

---
+++ Threads4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Threads4
+{
+    class MyClass
+    {
+        //объект блокировки(это может быть любой объект)
+        object block = new object();
+        public void Method()
+        {
+            int hash = Thread.CurrentThread.GetHashCode();
+
+            Monitor.Enter(block);
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Поток (бегун) # {hash}: итерация (круг) {counter}");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(new string('-', 37));
+
+            Monitor.Exit(block);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 40);
+
+            MyClass instance = new MyClass();
+
+            //Создание потоков по слабой ссылке
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(instance.Method).Start();
+            }
+            Thread.Sleep(1000);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Threads5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Threads5
+{
+    public class Program
+    {
+
+        static object block = new object();
+        static void WriteSecond()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                lock (block)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(new string(' ', 10)+ "Вторичный" );
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Thread.Sleep(100);
+                }
+            }
+
+
+
+            
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteSecond);
+            thread.Start();
+            for (int i = 0; i < 20; i++)
+            {
+                lock(block)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Первичный");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Thread.Sleep(100);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
